669f5526d445e9d9f64e6b819c034ba1c574ee77
Sunil Gudivada <SunilGudivada@users.noreply.github.com>
2020-09-14 14:05:42 +0530

    Added leet code problems

---
+++ src/com/company/leetCode/MaximumProductSubArray.java
@@ -0,0 +1,36 @@
+package com.company.leetCode;
+
+import jdk.dynalink.beans.StaticClass;
+
+import java.util.HashSet;
+
+public class MaximumProductSubArray {
+
+  public static int get(int[] nums){
+    int product = 1;
+    int maxProduct = Integer.MIN_VALUE;
+
+    for(int i=0;i<nums.length;i++){
+      product *=nums[i];
+      maxProduct = Math.max(maxProduct,product);
+      if(product == 0){
+        product = 1;
+      }
+    }
+    product = 1;
+    for(int i= nums.length-1;i>=0;i--){
+      product *=nums[i];
+      maxProduct = Math.max(maxProduct,product);
+      if(product == 0){
+        product = 1;
+      }
+    }
+    return maxProduct;
+  }
+
+  public static void main(String[] args){
+    int[] nums = {3, -1, 4};
+    System.out.println(get(nums));
+
+  }
+}
