e7c2795c0e14bbac37b34998985fc5047bb729e8
Sunil Gudivada <SunilGudivada@users.noreply.github.com>
2020-09-16 21:42:30 +0530

    Added leet code problems

---
+++ src/com/company/geeksForGeeks/countSetBits.java
@@ -0,0 +1,14 @@
+package com.company.geeksForGeeks;
+
+// Function to count set bits in the given number x
+// n: input to count the number of set bits
+
+public class countSetBits {
+  public static void main(String[] args){
+    int n =4;
+    int count = 0;
+    for(int i=1;i<=n;i++){
+
+    }
+  }
+}
