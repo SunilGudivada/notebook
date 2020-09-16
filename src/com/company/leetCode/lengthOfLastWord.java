ec7783f0ea0cefa4fe1622b5f197d3128dd169a1
Sunil Gudivada <SunilGudivada@users.noreply.github.com>
2020-09-17 00:52:08 +0530

    Added single line solution to length of longest word

--- src/com/company/leetCode/lengthOfLastWord.java
+++ src/com/company/leetCode/lengthOfLastWord.java
@@ -1,8 +1,15 @@
 package com.company.leetCode;
 
 class Solution {
+
+  //two liner
   public int lengthOfLastWord(String s) {
     String[] sa = s.split(" ");
     return sa.length >0 ? sa[sa.length-1].length() : 0;
+  }
+
+  //one Liner
+  public int lengthofLastWordOneLiner(String s){
+    return s.trim().length() - s.trim().lastIndexOf(" ") - 1;
   }
 }
