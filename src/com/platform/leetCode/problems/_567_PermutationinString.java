8cd872bb5c619557e93f24c214ea5a58b7e37a18
Sunil Gudivada <sunilgudivada369@gmail.com>
2021-01-03 20:01:30 +0530

    Added leet code problems

--- src/com/platform/leetCode/problems/_567_PermutationinString.java
+++ src/com/platform/leetCode/problems/_567_PermutationinString.java
@@ -1,26 +1,27 @@
 package com.platform.leetCode.problems;
 
-import java.util.HashMap;
-
 /**
  * Given two strings s1 and s2, write a function to return true if s2 contains the permutation of
  * s1. In other words, one of the first string's permutations is the substring of the second string.
  *
  * Example 1:
  *
- * Input: s1 = "ab" s2 = "eidbaooo" Output: True Explanation: s2 contains one permutation of s1
- * ("ba").
+ * Input: s1 = "ab" s2 = "eidbaooo"
+ * Output: True
+ * Explanation: s2 contains one permutation of s1 ("ba").
  *
  * Example 2:
  *
- * Input:s1= "ab" s2 = "eidboaoo" Output: False
+ * Input:s1= "ab" s2 = "eidboaoo"
+ * Output: False
  *
  * Constraints:
  *
- * The input strings only contain lower case letters. The length of both given strings is in
- * range [1, 10,000].
+ * The input strings only contain lower case letters.
+ * The length of both given strings is in range [1, 10,000].
  */
 public class _567_PermutationinString {
+  // sliding window method
   public boolean checkInclusion(String s1, String s2) {
     if (s1.length() > s2.length() || s2.length() == 0) return false;
     int[] s1Arr = new int[26];
